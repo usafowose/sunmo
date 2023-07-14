0a40fd17f65ba9d63316f21dbbbab69b4dd71246
Andrew Fowose <usafowose@gmail.com>
2023-07-14 02:55:31 -0400

    adding more controller methods for users

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -26,7 +26,8 @@ export const getAllUsers = async (): Promise<User[]> => {
 // user clicks a profile in tree to view more info.. this profile is already mapped to id so we can grab profile directly from id
 export const getUserById = async (userId: string): Promise<User> => {
   try {
-    
+    const user: User = await userService.getUserById(userId);
+    return user;
   } catch (err) {
     if (err.code) {
       if (err.message) {
@@ -42,9 +43,10 @@ export const getUserById = async (userId: string): Promise<User> => {
   }
 }
 
-export const getAllUnregisteredUsers = async (): Promise<User[]> => {
+export const getPendingUsers = async (): Promise<User[]> => {
   try {
-    
+    const unregisteredUsers: User[] = await userService.getPendingUsers();
+    return unregisteredUsers.length ? unregisteredUsers : null;
   } catch (err) {
     if (err.code) {
       if (err.message) {
@@ -60,9 +62,50 @@ export const getAllUnregisteredUsers = async (): Promise<User[]> => {
   }
 }
 
-export const getUserByLastName = async (): Promise<User[]> => {
+export const getUserByLastName = async (lastName: string): Promise<User[]> => {
   try {
-    
+    const users: User[] = await userService.getUserByAttribute('last_name', lastName);
+    return users;
+  } catch (err) {
+    if (err.code) {
+      if (err.message) {
+        throw new APIError(err.message, err.code);
+      }
+      throw new APIError(ErrorService.defaultErrorMessage, err.code)
+    } else {
+      if (err.message) {
+        throw new APIError(err.message, 500);
+      }
+      throw ErrorService.defaultError();
+    }
+  }
+}
+
+export const resetUserPassword = (userId: string, newPassword): Promise<void> => {
+    return Promise.resolve();
+}
+
+export const changeUserName = async (newUserName: string): Promise<void> => {
+  try {
+    await userService.changeUserName(newUserName);
+  } catch (err) {
+    if (err.code) {
+      if (err.message) {
+        throw new APIError(err.message, err.code);
+      }
+      throw new APIError(ErrorService.defaultErrorMessage, err.code)
+    } else {
+      if (err.message) {
+        throw new APIError(err.message, 500);
+      }
+      throw ErrorService.defaultError();
+    }
+  }
+}
+
+export const changeUserEmail = async (newEmail: string): Promise<void> => {
+  try {
+    await userService.changeUserEmail(newEmail);
   } catch (err) {
     if (err.code) {
       if (err.message) {
