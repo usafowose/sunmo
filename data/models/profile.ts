6dbfbeec06c9f453198519105191ff161b3b76e4
Andrew Fowose <usafowose@gmail.com>
2023-07-14 03:09:52 -0400

    adding models for profile for in-app use and data transformation

---
+++ data/models/profile.ts
@@ -0,0 +1,48 @@
+import { User } from "./orm-entities/userentity";
+
+export interface Profile {
+  profileId: string;
+  user: User;
+  userName?: string;
+  displayName?: string;
+  profileIcon?: string;
+  metadata?: ProfileData;
+}
+
+export interface ProfileData {
+  createdAt?: Date;
+  source?: RegistrationSource;
+  lastUpdated: Date;
+  lastSeen: Date;
+}
+
+export interface RawUser {
+  firstName: string;
+  surname: string;
+  birthday: Date;
+  contact: UserContact;
+}
+
+export interface UserContact {
+  email: string;
+  number?: string;
+  city?: string;
+  country?: string;
+}
+
+export interface ProfileResult {
+  id: number;
+  user_id: number;
+  first_name: string;
+  last_name: string;
+  birthday: Date;
+  email:string;
+  [key: string]: any;
+}
+
+export enum RegistrationSource {
+  Invitation = 0,
+  SelfRegistration = 1
+}
+
+export type CreateUserInput = Pick<User, "email" | "first_name" | "last_name"> & Partial<Pick<User, 'dob'>>
