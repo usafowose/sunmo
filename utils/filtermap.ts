aa74841a6932ee4d7d084b52fe229a5b8ab88ddc
Andrew Fowose <usafowose@gmail.com>
2023-08-03 17:46:24 -0400

    moved and added utils

---
+++ utils/filtermap.ts
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param reqObj - An object, either request or custom, from which specific key-value pairs must be extracted.
+ * @param validKeyMappingMock - A mock object whose enumerable keys constitute all valid keys to extract.
+ * @returns filters: A JS Map() of type Map<T, any> in which the Map key type is the same as the provided type OR corresponds to keyof validKeyMappingMock.
+ */
+export const createFilterMapFromRequest = <T>(
+  reqObj: Record<string, any>,
+  validKeyMappingMock: Record<string, any>,
+): Map<T, any> => {
+  const filters: Map<T, any> = new Map();
+  Object.entries(reqObj).forEach(([key, value]) => {
+    if (key in validKeyMappingMock) {
+      filters.set(key as unknown as T, value);
+    }
+  });
+  return filters;
+}
