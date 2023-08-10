2b45f311bf52633c331c436a66f42cfc2c53225d
Andrew Fowose <usafowose@gmail.com>
2023-08-09 22:06:27 -0400

    adding user creation capability with existence check

--- routes/routers/user.ts
+++ routes/routers/user.ts
@@ -1,16 +1,22 @@
 import { Router } from "express";
 import * as express from 'express';
-import { getUsersHandler, getUnregisteredUsersHandler, getUserByIdHandler, /* updateEmailHandler */ } from "../handlers";
+import { getUsersHandler, getUnregisteredUsersHandler, getUserByIdHandler, createNewUserHandler, /* updateEmailHandler */ } from "../handlers";
 
 export class UserRouter {
   private static _router: Router = express.Router();
 
+  /**
+   * @summary All routes are subroutes under the prepended route path '/users
+   */
   static get router(): Router {
-    // this._router.use() // any middleware specific to this route like auth,
+    // this._router.use(authMiddleWare) // any middleware specific to this route like auth,
 
-    this._router.get('/', [getUsersHandler]);
+    this._router.get('/', [/*authMiddleware*/ getUsersHandler]);
     this._router.get('/unregistered', [getUnregisteredUsersHandler]);
     this._router.get('/:id', [getUserByIdHandler]);
+
+    this._router.post('/', [createNewUserHandler]);
+
 
     // this._router.post('/reset-password', [/*authMiddleware*/, /*tokenMiddleware*/]);
     // this._router.post('/update-password', [/*authMiddleware*/, /*tokenMiddleware*/]);
